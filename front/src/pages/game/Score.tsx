fb0d2687984eef02475fca314f06e85c08a440e9
9utty <leegh4250@gmail.com>
2023-05-30 02:48:05 +0900

    feat: 게임스코어 추가

--- front/src/pages/game/Score.tsx
+++ front/src/pages/game/Score.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Counter } from "react95";
 
-const GameScore = () => {
-  return <div></div>;
+interface Score {
+  player1: number;
+  player2: number;
+}
+
+const GameScore = ({ player1, player2 }: Score) => {
+  return (
+    <div>
+      <Counter size={"md"} value={player1} />
+      <Counter size={"md"} value={player2} />
+    </div>
+  );
 };
 
 export default GameScore;
