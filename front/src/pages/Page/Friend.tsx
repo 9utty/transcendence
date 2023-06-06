465ba42f3acc1f2f63e163d1804dcc41f2dd4f6b
9utty <leegh4250@gmail.com>
2023-06-06 19:17:44 +0900

    feat: 랜덤매칭

--- front/src/pages/Page/Friend.tsx
+++ front/src/pages/Page/Friend.tsx
@@ -7,6 +7,7 @@ import FriendUser from "../friendList/components/FriendUser";
 import AppLayout from "../globalComponents/AppLayout";
 import MyModal from "../globalComponents/MyModal";
 import { useRouter } from "next/router";
+import H3 from "../PostComponents/H3";
 
 const FriendList = () => {
   const [state, setState] = useState({ activeTab: 0 });
@@ -81,6 +82,7 @@ const FriendList = () => {
                 mocUserData.map((user, index) => (
                   <BlockUser key={index} userNickName={user.userNickName} />
                 ))}
+              {activeTab === 2 && <H3>검색화면</H3>}
             </ScrollView>
           </Row>
         </WindowContent>
--- front/src/pages/Page/Random.tsx
+++ front/src/pages/Page/Random.tsx
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import AppLayout from "../globalComponents/AppLayout";
 import MyModal from "../globalComponents/MyModal";
 import { useRouter } from "next/router";
-import { WindowContent } from "react95";
+import { Button, WindowContent } from "react95";
+import H3 from "../PostComponents/H3";
+import { useSelector } from "react-redux";
+import RootState from "@/redux/RootReducer";
+
+interface User {
+  uId: number;
+  uNickName: string;
+}
 
 const Random = () => {
   const router = useRouter();
+  const [isMatch, setIsMatch] = useState(false);
+  const [guestUser, setGuestUser] = useState<User>({ uId: 0, uNickName: "" });
+  const host = useSelector((state: RootState) => state.user);
 
   const close = () => {
     router.back();
   };
 
+  const Match = () => {
+    // TODO : 랜덤 요청
+    // isMatch 바꿔주기
+  };
+
   return (
     <AppLayout>
       <MyModal hName="랜덤매칭" close={close}>
-        <WindowContent></WindowContent>
+        <WindowContent style={{ marginTop: "150px" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <H3>{host.nickName === "" ? `플레이어1` : `${host.nickName}`}</H3>
+            </div>
+            <div>
+              <H3>
+                {guestUser.uNickName === ""
+                  ? `플레이어?`
+                  : `${guestUser.uNickName}`}
+              </H3>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "40px",
+            }}
+          >
+            <Button
+              style={{
+                width: "10vw",
+              }}
+              onClick={Match}
+            >
+              <H3>{isMatch ? `시작` : `매칭`}</H3>
+            </Button>
+          </div>
+        </WindowContent>
       </MyModal>
     </AppLayout>
   );
